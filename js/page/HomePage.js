449c98d76fa113d7b33fa859f4e70301b6083029
a153722219 <437816510@qq.com>
2019-10-20 12:27:56 +0800

    [fix]物理返回键

--- js/page/HomePage.js
+++ js/page/HomePage.js
@@ -8,15 +8,40 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import {BackHandler} from "react-native";
+import {connect} from 'react-redux'
 import {
   createBottomTabNavigator,
-  createAppContainer
+  createAppContainer,
+  NavigationActions
 } from 'react-navigation'
 
 import NavigationUtil from '../navigator/NavigationUtil'
 import DynamicTabNavigator from '../navigator/DynamicTabNavigator'
-export default class HomePage extends Component{
+ class HomePage extends Component{
 
+
+    componentDidMount() {
+        BackHandler.addEventListener("hardwareBackPress", this.onBackPress);
+    }
+
+    componentWillUnmount() {
+        BackHandler.removeEventListener("hardwareBackPress", this.onBackPress);
+    }
+    /**
+     * 处理 Android 中的物理返回键
+     * https://reactnavigation.org/docs/en/redux-integration.html#handling-the-hardware-back-button-in-android
+     * @returns {boolean}
+     */
+    onBackPress = () => {
+        const {dispatch, nav} = this.props;
+        //if (nav.index === 0) {
+        if (nav.routes[1].index === 0) {//如果RootNavigator中的MainNavigator的index为0，则不处理返回事件
+            return false;
+        }
+        dispatch(NavigationActions.back());
+        return true;
+    };
 
   render() {
         NavigationUtil.navigation = this.props.navigation;
@@ -27,16 +52,8 @@ export default class HomePage extends Component{
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  }
+const mapStateToProps = state => ({
+    nav: state.nav,
 });
+
+export default connect(mapStateToProps)(HomePage);
