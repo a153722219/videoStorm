0273354d6a2f4630be421c816723eeb77663cd52
a153722219 <437816510@qq.com>
2020-04-24 17:05:38 +0800

    定位

--- js/util/constant.js
+++ js/util/constant.js
@@ -15,4 +15,4 @@ export const APPKEY = 'BC794515-B998-4608-829E-CC4450931D66';
 
 export const Google_API_KEY = 'AIzaSyBGehdVw7d9FI4ZvCHdvPYG6vahPjl2NcM';
 
-export const proxyUrl = "https://www.baidream.top/";
+export const proxyUrl = "https://maps.googleapis.com/";
